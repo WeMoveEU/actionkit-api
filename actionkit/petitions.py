4c2e49c655d1c7b3ae70f24e230843e47b1d9ed4
Romain Thouvenin <romain@wemove.eu>
2022-12-13 10:56:28 +0100

    Support for petition page content on creation (still need followup data)

--- actionkit/petitions.py
+++ actionkit/petitions.py
@@ -2,11 +2,15 @@ class Petitions:
     def __init__(self, connection):
         self.connection = connection
 
-    def create(self, **kwargs):
-        return self.connection.get(self.connection.post("petitionpage/", kwargs))
+    def create(self, page, content):
+        page_uri = self.connection.post("petitionpage", page)
+        content = dict(content)
+        content["page"] = page_uri
+        self.connection.post("petitionform", content)
+        return page_uri
 
     def get(self, id):
         return self.connection.get(f"petitionpage/{id}/")
 
-    def patch(self, id, **params):
+    def update(self, id, params):
         return self.connection.patch(f"petitionpage/{id}/", params)
