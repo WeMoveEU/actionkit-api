50cd981d24fe56e496e521e151946b5050645cdf
Romain Thouvenin <romain@wemove.eu>
2022-12-13 10:55:50 +0100

    Languages by code utility function

--- actionkit/languages.py
+++ actionkit/languages.py
@@ -5,6 +5,10 @@ class Languages(HttpMethods):
     def __init__(self, connection):
         self.connection = connection
 
+    def by_code(self):
+        langs = self.connection.get("language/")
+        return dict(map(lambda l: (l["iso_code"], l), langs["objects"]))
+
     def uris(self):
         langs = self.connection.get("language/")
         return dict(map(lambda l: (l["iso_code"], l["resource_uri"]), langs["objects"]))
