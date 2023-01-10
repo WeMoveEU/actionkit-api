b4dca56fef4059efb6bd99a8b118cb5dfa0728e3
Romain Thouvenin <romain@wemove.eu>
2023-01-10 15:39:01 +0100

    Utilities for users API

--- actionkit/users.py
+++ actionkit/users.py
@@ -1,19 +1,30 @@
-from .httpmethods import HttpMethods
+import re
 
+from .httpmethods import HttpMethods
 
 class Users(HttpMethods):
     def __init__(self, connection):
         self.connection = connection
 
     def get(self, email):
-        users = self.connection.get(f"user/?email={email}")
+        users = self.connection.get(f"{self.uri()}?email={email}")
         if users["meta"]["total_count"] == 0:
             return None
         else:
             return users["objects"][0]
 
     def create(self, user):
-        return self.connection.post("user/", user)
+        return self.connection.post(self.uri(), user)
 
     def update(self, path, user):
         return self.connection.patch(path, user)
+
+    def uri(self, id=None):
+        return f"user/{id or ''}"
+
+    def id(self, uri):
+        m = re.search("/user/(\d+)", uri)
+        if m:
+            return m[1]
+        else:
+            raise Exception(f"{uri} is not a user URI")
