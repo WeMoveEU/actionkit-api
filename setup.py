8d2f023a21cd526a683a3b3d3cb3a26fd67b560c
Romain Thouvenin <romain@wemove.eu>
2023-01-19 10:51:16 +0100

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,5 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["Click", "requests", "requests-toolbelt"],
-    # entry_points={
-    #     "console_scripts": [
-    #         "donation-import=actionkit.scripts.donation-import:cli",
-    #     ],
-    # },
+    install_requires=["requests", "requests-toolbelt"],
 )
