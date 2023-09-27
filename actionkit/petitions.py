f07bcd5599dffbe425da27f44068b1a516a59400
Romain Thouvenin <romain@wemove.eu>
2023-09-27 12:25:52 +0200

    Improve petition creation from model

--- actionkit/petitions.py
+++ actionkit/petitions.py
@@ -9,17 +9,21 @@ class Petitions(HttpMethods):
 
         content = dict(content)
         content["page"] = page_uri
-        self.connection.post("petitionform", json=content)
+        response = self.connection.post("petitionform", json=content)
+        cms_form_uri = self.get_resource_uri(response)
 
         followup = dict(followup)
         followup["page"] = page_uri
-        self.connection.post("pagefollowup", json=followup)
+        response = self.connection.post("pagefollowup", json=followup)
+        followup_uri = self.get_resource_uri(response)
 
-        return page_uri
+        return (page_uri, cms_form_uri, followup_uri)
 
     def create_from_model(self, model, page, content, followup):
-        new_page = dict(page)
+        base_page = {k: model[k] for k in ["language", "goal", "goal_type"]}
+        new_page = base_page | page
         new_page["fields"] = model["fields"] | page["fields"]
+        new_page["groups"] = new_page.get("groups", []) + [g["resource_uri"] for g in model["groups"]]
 
         new_content = {
             k: model["cms_form"][k]
@@ -35,17 +39,20 @@ class Petitions(HttpMethods):
 
         new_followup = {
             k: model["followup"][k]
-            for k in [
-                "twitter_message",
-                "share_description",
-                "email_body",
-                "email_from_line",
-                "email_subject",
-                "email_wrapper",
-            ]
+            for k in model["followup"] if k not in ["id", "page", "resource_uri", "url"]
         }
         new_followup |= followup
-        return self.create(new_page, new_content, new_followup)
+        uris = self.create(new_page, new_content, new_followup)
+
+        model_form_id = self.get_resource_uri_id(model["cms_form"]["resource_uri"])
+        cms_form_id = self.get_resource_uri_id(uris[1])
+        form_fields = self.connection.get("userformfield", params=dict(form_id=model_form_id, form_type=333)).json().get("objects")
+        for field in form_fields:
+            new_field = {k: field[k] for k in field if k not in ["id", "form_id", "created_at", "updated_at", "resource_uri"]}
+            new_field["form_id"] = cms_form_id
+            self.connection.post("userformfield", new_field)
+
+        return uris
 
     def get(self, id):
         return super().get(f"petitionpage/{id}/")
