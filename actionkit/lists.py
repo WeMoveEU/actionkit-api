5aaa674ae20deed916e6b6e40f1d36c5bf63c8da
Romain Thouvenin <romain@wemove.eu>
2023-10-19 10:11:34 +0200

    Allow adding notes to mailing lists

--- actionkit/lists.py
+++ actionkit/lists.py
@@ -4,13 +4,13 @@ from .httpmethods import HttpMethods
 class Lists(HttpMethods):
     resource_name = "list"
 
-    def get_or_create(self, list_name):
+    def get_or_create(self, list_name, notes=None):
         """Find or create a list."""
         lists = self.get(name=list_name)
 
         if lists["meta"]["total_count"] == 0:
             # sigh, terrible design. second request to get the object we just created. bad past aaron.
-            return self.get(self.post({"name": list_name}))
+            return self.get(self.post({"name": list_name, "notes": notes}))
 
         return lists["objects"][0]
 
