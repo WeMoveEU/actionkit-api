d6efa9830e473a80a627b23ac594add54e9f302d
Tim Godfrey <timmygee@users.noreply.github.com>
2024-02-06 16:35:16 +1100

    Add update facility to genericactions

--- actionkit/genericactions.py
+++ actionkit/genericactions.py
@@ -3,3 +3,31 @@ from .httpmethods import HttpMethods
 
 class GenericActions(HttpMethods):
     resource_name = "action"
+
+    def update(
+        self,
+        resource_uri: str = None,
+        resource_id: str = None,
+        **kwargs: dict,
+    ) -> dict:
+        """
+        Update an generic action
+        """
+        if not (resource_id or resource_uri):
+            raise ValueError('Either resource_id or resource_uri must be provided')
+
+        if not resource_uri:
+            resource_uri = self.get_resource_uri_from_id(resource_id)
+
+        fields = kwargs.pop('fields', {})
+
+        payload = kwargs.copy()
+
+        if fields:
+            # We gotta update the custom action fields so we need to fetch what is there first
+            action = self.get(resource_uri)
+            action_fields = action.get('fields', {})
+            action_fields.update(fields)
+            payload['fields'] = action_fields
+
+        return self.patch(resource_uri, payload)
--- actionkit/orders.py
+++ actionkit/orders.py
@@ -8,19 +8,22 @@ class Orders(HttpMethods):
 
     def update(
         self,
-        order_uri: str = None,
-        order_id: str = None,
+        resource_uri: str = None,
+        resource_id: str = None,
         total: Decimal = None,
         **kwargs: dict
     ) -> dict:
         """
         Update an order
         """
-        if not (order_id or order_uri):
-            raise ValueError('Either order_id or order_uri must be provided')
+        if not (resource_id or resource_uri):
+            raise ValueError('Either resource_id or resource_uri must be provided')
+
+        if not resource_uri:
+            resource_uri = self.get_resource_uri_from_id(resource_id)
 
         payload = kwargs.copy()
         if total is not None:
             payload['total'] = str(total)
 
-        return self.patch(order_uri, to_patch=payload)
+        return self.patch(resource_uri, to_patch=payload)
